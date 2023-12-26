3caa1f0823d857c5c3471ecea17680c2276a8434
manish1513 <gurjarmanish1513@gmail.com>
2023-12-26 09:36:40 +0530

    crudcrud using get

--- script.js
+++ script.js
@@ -1,83 +1,77 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('userForm');
-    const submitBtn = document.getElementById('submitBtn');
-    const userList = document.getElementById('userList');
+const form = document.getElementById('userForm');
+const submitBtn = document.getElementById('submitBtn');
+const userList = document.getElementById('userList');
 
-    form.addEventListener('submit', function (event) {
-        event.preventDefault();
-        addUser();
-    });
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    addUser();
+});
 
-    submitBtn.addEventListener('click', addUser);
+submitBtn.addEventListener('click', addUser);
 
-    function addUser() {
-        const name = document.getElementById('name').value;
-        const email = document.getElementById('email').value;
-        const phone = document.getElementById('phone').value;
+function addUser() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
 
-        const userDetail = {
-            name: name,
-            email: email,
-            phone: phone
-        };
+    const userDetail = {
+        name: name,
+        email: email,
+        phone: phone
+    };
 
-        axios.post("https://crudcrud.com/api/7e2e567a413a465d86db8f63c99306cf/appointmentData", userDetail)
+    axios.post("https://crudcrud.com/api/13b1fc5cc0b347cfb8b0e50ad2cdd54a/appointmentData", userDetail)
         .then((response) => {
-            displayUserList(response.data)
-            //console.log(response)
+            displayUserList(response.data); 
         })
         .catch((err) => {
-        document.body.innerHTML = document.body.innerHTML + "<h4> something went wrong </h4>"
-            console.log(err)
-        });
-    
-       // localStorage.setItem(email, JSON.stringify(userDetail));
-       // displayUserList();
-        form.reset();
-    }
-
-    function displayUserList() {
-        userList.innerHTML = "";
-
-        for (let i = 0; i < localStorage.length; i++) {
-            const email = localStorage.key(i);
-            const data = JSON.parse(localStorage.getItem(email));
-
-            const li = document.createElement('li');
-            li.innerHTML = `${data.name} - ${data.email} - ${data.phone} <button class="edit-btn" data-email="${email}">Edit</button> <button class="delete-btn" data-email="${email}">Delete</button>`;
-            userList.appendChild(li);
-        }
-
-        // Add event listeners for edit and delete buttons
-        const editButtons = document.querySelectorAll('.edit-btn');
-        editButtons.forEach(button => {
-            button.addEventListener('click', editUser);
+            document.body.innerHTML = document.body.innerHTML + "<h4> Something went wrong </h4>";
+            console.log(err);
         });
 
-        const deleteButtons = document.querySelectorAll('.delete-btn');
-        deleteButtons.forEach(button => {
-            button.addEventListener('click', deleteUser);
+    form.reset();
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    axios.get("https://crudcrud.com/api/13b1fc5cc0b347cfb8b0e50ad2cdd54a/appointmentData")
+        .then((response) => {
+            console.log(response);
+            for (var i = 0; i < response.data.length; i++) {
+                const user = response.data[i];
+                displayUserList(user);
+            }
+        })
+        .catch((err) => {
+            console.log(err);
         });
-    }
-
-    function editUser(event) {
-        const email = event.target.dataset.email;
-        const data = JSON.parse(localStorage.getItem(email));
-
-        document.getElementById('name').value = data.name;
-        document.getElementById('email').value = data.email;
-        document.getElementById('phone').value = data.phone;
-
-        localStorage.removeItem(email);
-        displayUserList();
-    }
-
-    function deleteUser(event) {
-        const email = event.target.dataset.email;
-        localStorage.removeItem(email);
-        displayUserList();
-    }
-
-    // Display user list on page load
-    displayUserList();
 });
+
+function displayUserList(user) {
+    // Create a new list item for each user
+    const li = document.createElement('li');
+    const email = user.email;
+    li.innerHTML = `${user.name} - ${user.email} - ${user.phone} <button class="edit-btn" onclick="editUser('${email}')">Edit</button> <button class="delete-btn" onclick="deleteUser('${email}')">Delete</button>`;
+
+    // Append the new list item to the user list
+    userList.appendChild(li);
+
+    // Add event listeners for edit and delete buttons
+    const editButtons = document.querySelectorAll('.edit-btn');
+    editButtons.forEach(button => {
+        button.addEventListener('click', editUser);
+    });
+
+    const deleteButtons = document.querySelectorAll('.delete-btn');
+    deleteButtons.forEach(button => {
+        button.addEventListener('click', deleteUser);
+    });
+}
+
+function editUser(event) {
+    // Handle edit logic here using data from the API
+    // Update the input fields with the existing user details
+}
+
+function deleteUser(event) {
+    displayUserList([]);
+}
